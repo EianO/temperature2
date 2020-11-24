8660f040a61de7213d5f8d68c906a6a13e9e5d19
EianO <72766349+EianO@users.noreply.github.com>
2020-11-24 09:54:33 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print("temperature"+input.temperature(TemperatureUnit.FAHRENHEIT))
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 70:
+        light.set_pixel_color(5,light.rgb(255, 0, 0))
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) > 40:
+        light.set_pixel_color(5,light.rgb(0, 0, 255))
+    else:
+        light.set_pixel_color(5,light.rgb(0, 0, 255))
+       
